87bb0cf058921348dc696f33061a8e9334e9c437
gravikiran <gravikiran.2012@gmail.com>
2015-01-23 12:13:42 +0530

    committing cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions are written to 
+## handle the time overhead and to avoid repeated computations
+## It saves the time by storing in cache of already computed inverse 
+## of a matrix-which can be re-used for the same matrix - instead 
+## of recomputing
 
-## Write a short comment describing this function
+
+##function makeCacheMatrix   
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inverse <<- solve
+    getsolve <- function() inverse
+    list(set = set, get = get, 
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getsolve()
+    if(!is.null(inverse)){
+        message("getting cached data")
+    }
+    inverseData <- x$get()
+    inverse <- solve(inverseData, ...)
+    x$setsolve(inverse)
+    inverse
+
 }
